3ee9ee0a39383b5b2cb76e9380709d7489052392
alirazabilal <129405525+alirazabilal@users.noreply.github.com>
2024-10-07 19:23:41 +0500

    Update App.jsx for all components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,28 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 // import "./App.css";
 import Navbar from "./components/Navbar";
+import Listings from "./components/Listings";
+import Footer from "./components/Footer";
+import Categories from "./components/Categories";
 function App() {
+  const [count, setCount] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleCategorySelect = (category) => {
+    if (category === "All Listings") {
+      setSelectedCategory("");
+    } else {
+      setSelectedCategory(category);
+    }
+  };
   return (
     <>
       <Navbar />
+      <div>
+        <Categories onCategorySelect={handleCategorySelect} />
+        <Listings selectedCategory={selectedCategory} />
+      </div>
+      <Footer />
     </>
   );
 }
