aac3ba1ee11ed4f57d5d1ffb0bae1700f4bc2bbc
alirazabilal <129405525+alirazabilal@users.noreply.github.com>
2024-10-07 11:27:58 +0500

    ADD FOOTER HTML 1

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1 className="heading">Inspiration for future getaways</h1>
+        <div className="links">
+          <a href="#">Popular</a>
+          <a href="#">Arts & culture</a>
+          <a href="#">Outdoors</a>
+          <a href="#">Mountains</a>
+          <a href="#">Beach</a>
+          <a href="#">Unique stays</a>
+          <a href="#">Categories</a>
+          <a href="#">Things to do</a>
+          <a href="#">Travel tips & inspiration</a>
+          <a href="#">Airbnb-friendly apartments</a>
+        </div>
+        <hr />
+        <div className="links2">
+          <a href="#">
+            <b>Canmore</b> <br />
+            Apartment rentals
+          </a>
+          <a href="#">
+            {" "}
+            <b>Benalmádena</b> <br />
+            Apartment rentals
+          </a>
+          <a href="#">
+            <b>Marbella</b> <br />
+            Apartment rentals
+          </a>
+          <a href="#">
+            <b>Mijas</b> <br />
+            Apartment rentals
+          </a>
+          <a href="#">
+            <b>Prescott</b> <br />
+            Pet-friendly rentals
+          </a>
+          <a href="#">
+            <b>Scottsdale</b> <br /> House rentals
+          </a>
+        </div>
+
+        <div className="links3">
+          <a href="#">
+            <b>Canmore</b> <br />
+            Apartment rentals
+          </a>
+          <a href="#">
+            {" "}
+            <b>Benalmádena</b> <br />
+            Apartment rentals
+          </a>
+          <a href="#">
+            <b>Marbella</b> <br />
+            Apartment rentals
+          </a>
+          <a href="#">
+            <b>Mijas</b> <br />
+            Apartment rentals
+          </a>
+          <a href="#">
+            <b>Prescott</b> <br />
+            Pet-friendly rentals
+          </a>
+          <a href="#">
+            <b>Scottsdale</b> <br /> House rentals
+          </a>
+        </div>
+
+        <hr />
+        <div className="cont">
+          <div className="links-a">
+            <p>© 2024 Airbnb, Inc.·TermsSitemapPrivacyYour Privacy Choices</p>
+          </div>
+          <div className="links-right">
+            <i class="globe fa-solid fa-globe"></i>
+            English (US)
+            <a href="https://www.facebook.com/ali.razabilal.98">
+              {" "}
+              <i class="fb fa-brands fa-facebook"></i>
+            </a>
+            <a href="https://www.instagram.com/ali_raza_bilal_/">
+              {" "}
+              <i class="insta fa-brands fa-instagram"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
